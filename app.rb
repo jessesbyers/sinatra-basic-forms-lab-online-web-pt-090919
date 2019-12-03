66bd378276b845ea9403a6dd2818c06cdc409e6b
Jesse Byers <jesse.s.byers@gmail.com>
2019-12-03 18:30:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,10 +3,6 @@ require_relative 'models/puppy'
 
 class App < Sinatra::Base
 
-#
-# The home page will also need a new view index.erb.
-# This page should welcome you to the Puppy Adoption Site.
-# Add this view to the controller action.
   get "/" do
     erb :index
   end
